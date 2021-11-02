ebfb555ec83105707757b5c759beff2f9a9ed6e8
Kevin Zhao <kzhao0986@gmail.com>
2021-11-02 08:34:05 -0700

    More tests

--- crates/parsimon-core/src/network/routing.rs
+++ crates/parsimon-core/src/network/routing.rs
@@ -80,8 +80,22 @@ mod tests {
     type SortedHopMatrix = BTreeMap<NodeId, SortedHopMap>;
     type SortedHopMap = BTreeMap<NodeId, Vec<NodeId>>;
 
+    /// Generate a stable sorting of the hop matrix for tests
     fn sorted_hop_matrix(matrix: &HopMatrix) -> SortedHopMatrix {
-        todo!()
+        matrix
+            .iter()
+            .map(|(&id, m)| {
+                let m = m
+                    .iter()
+                    .map(|(&id, hops)| {
+                        let mut hops = hops.clone();
+                        hops.sort();
+                        (id, hops)
+                    })
+                    .collect::<BTreeMap<_, _>>();
+                (id, m)
+            })
+            .collect::<BTreeMap<_, _>>()
     }
 
     #[test]
@@ -93,24 +107,179 @@ mod tests {
         let l2 = Link::new(n2.id, n3.id);
         let topo = Topology::new(&[n1, n2, n3], &[l1, l2]).context("failed to create topology")?;
         let routes = Routes::new(&topo);
-        insta::assert_yaml_snapshot!(routes, @r###"
+        let hops = sorted_hop_matrix(&routes.inner);
+        insta::assert_yaml_snapshot!(hops, @r###"
         ---
-        inner:
-          0:
-            2:
-              - 2
-            1:
-              - 2
-          2:
-            1:
-              - 1
-            0:
-              - 0
+        0:
           1:
-            0:
-              - 2
-            2:
-              - 2
+            - 2
+          2:
+            - 2
+        1:
+          0:
+            - 2
+          2:
+            - 2
+        2:
+          0:
+            - 0
+          1:
+            - 1
+        "###);
+        Ok(())
+    }
+
+    // TODO: Verify me
+    #[test]
+    fn route_eight_node_succeeds() -> anyhow::Result<()> {
+        // 4 hosts (IDs 0-3), 4 switches (IDs 4 and 5 are ToRs, IDs 6 and 7 are Aggs)
+        let hosts = (0..=3).map(|i| Node::new_host(NodeId::new(i)));
+        let switches = (4..=7).map(|i| Node::new_switch(NodeId::new(i)));
+        let nodes = hosts.chain(switches).collect::<Vec<_>>();
+        // Each ToR is connected to 2 hosts
+        let mut links = Vec::new();
+        links.push(Link::new(nodes[0].id, nodes[4].id));
+        links.push(Link::new(nodes[1].id, nodes[4].id));
+        links.push(Link::new(nodes[2].id, nodes[5].id));
+        links.push(Link::new(nodes[3].id, nodes[5].id));
+        // Each ToR is connected to both Aggs
+        links.push(Link::new(nodes[4].id, nodes[6].id));
+        links.push(Link::new(nodes[4].id, nodes[7].id));
+        links.push(Link::new(nodes[5].id, nodes[6].id));
+        links.push(Link::new(nodes[5].id, nodes[7].id));
+        let topo = Topology::new(&nodes, &links).context("failed to create topology")?;
+        let routes = Routes::new(&topo);
+        let hops = sorted_hop_matrix(&routes.inner);
+        insta::assert_yaml_snapshot!(hops, @r###"
+        ---
+        0:
+          1:
+            - 4
+          2:
+            - 4
+          3:
+            - 4
+          4:
+            - 4
+          5:
+            - 4
+          6:
+            - 4
+          7:
+            - 4
+        1:
+          0:
+            - 4
+          2:
+            - 4
+          3:
+            - 4
+          4:
+            - 4
+          5:
+            - 4
+          6:
+            - 4
+          7:
+            - 4
+        2:
+          0:
+            - 5
+          1:
+            - 5
+          3:
+            - 5
+          4:
+            - 5
+          5:
+            - 5
+          6:
+            - 5
+          7:
+            - 5
+        3:
+          0:
+            - 5
+          1:
+            - 5
+          2:
+            - 5
+          4:
+            - 5
+          5:
+            - 5
+          6:
+            - 5
+          7:
+            - 5
+        4:
+          0:
+            - 0
+          1:
+            - 1
+          2:
+            - 6
+            - 7
+          3:
+            - 6
+            - 7
+          5:
+            - 6
+            - 7
+          6:
+            - 6
+          7:
+            - 7
+        5:
+          0:
+            - 6
+            - 7
+          1:
+            - 6
+            - 7
+          2:
+            - 2
+          3:
+            - 3
+          4:
+            - 6
+            - 7
+          6:
+            - 6
+          7:
+            - 7
+        6:
+          0:
+            - 4
+          1:
+            - 4
+          2:
+            - 5
+          3:
+            - 5
+          4:
+            - 4
+          5:
+            - 5
+          7:
+            - 4
+            - 5
+        7:
+          0:
+            - 4
+          1:
+            - 4
+          2:
+            - 5
+          3:
+            - 5
+          4:
+            - 4
+          5:
+            - 5
+          6:
+            - 4
+            - 5
         "###);
         Ok(())
     }
