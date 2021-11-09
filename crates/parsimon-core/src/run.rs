2f937362c9c79eb4ed8ad4b3531442c54eeaee96
Kevin Zhao <kzhao0986@gmail.com>
2021-11-08 20:15:09 -0600

    sync

--- crates/parsimon-core/src/run.rs
+++ crates/parsimon-core/src/run.rs
@@ -1,5 +1,5 @@
 use crate::network::DelayNetwork;
-use crate::Spec;
+use crate::spec::{Spec, SpecError};
 
 /// The core `Parsimon` routine. This transforms a specification into a network of delay
 /// distributions.
@@ -7,6 +7,7 @@ use crate::Spec;
 /// This function returns an error if the provided mappings in the specification are invalid.
 pub fn run(spec: Spec) -> Result<DelayNetwork, Error> {
     // Validate mappings
+    let spec = spec.validate()?;
     // Build a SimNetwork
     // let simnet = spec.network.with_flows(todo!());
     // Use SimNetwork to run simulations
@@ -14,5 +15,8 @@ pub fn run(spec: Spec) -> Result<DelayNetwork, Error> {
     todo!()
 }
 
-#[derive(Debug)]
-pub enum Error {}
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    InvalidSpec(#[from] SpecError),
+}
