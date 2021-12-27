27b6dcf7b2f5c04796a7400bb72cf04ee7a452c3
Kevin Zhao <kzhao0986@gmail.com>
2021-12-27 14:29:36 -0600

    nit

--- crates/clustering-impls/src/distance.rs
+++ crates/clustering-impls/src/distance.rs
@@ -6,17 +6,17 @@ use crate::utils;
 pub fn max_wmape_xs(a: &[Flow], b: &[Flow]) -> f64 {
     let s_a = utils::percentiles_100(a, |f| f.size);
     let s_b = utils::percentiles_100(b, |f| f.size);
-    let s_mae = utils::wmape(
+    let s_wmape = utils::wmape(
         &s_a.iter().map(|x| x.into_f64()).collect::<Vec<_>>(),
         &s_b.iter().map(|x| x.into_f64()).collect::<Vec<_>>(),
     );
     let d_a = utils::percentiles_100(&utils::deltas(a), |&x| x);
     let d_b = utils::percentiles_100(&utils::deltas(b), |&x| x);
-    let d_mae = utils::wmape(
+    let d_wmape = utils::wmape(
         &d_a.iter().map(|x| x.into_f64()).collect::<Vec<_>>(),
         &d_b.iter().map(|x| x.into_f64()).collect::<Vec<_>>(),
     );
-    std::cmp::max_by(s_mae, d_mae, |x, y| {
+    std::cmp::max_by(s_wmape, d_wmape, |x, y| {
         x.partial_cmp(y)
             .expect("`max_mae_xs`: failed to compare floats")
     })
