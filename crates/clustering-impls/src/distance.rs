4c199f80de7bf8e776e8ac08ce2eedcbd021cd0c
Kevin Zhao <kzhao0986@gmail.com>
2022-01-04 12:35:33 -0500

    clustering-impls: comment out max_mae_rescaled_xs

--- crates/clustering-impls/src/distance.rs
+++ crates/clustering-impls/src/distance.rs
@@ -17,15 +17,15 @@ pub fn max_wmape_xs(a: &[Flow], b: &[Flow]) -> f64 {
 }
 
 // PRECONDITION: flows in `a` and `b` are sorted by start time
-pub fn max_mae_rescaled_xs(a: &[Flow], b: &[Flow]) -> f64 {
-    let s_a = utils::percentiles(a, |f| f.size);
-    let s_b = utils::percentiles(b, |f| f.size);
-    let s_mae = utils::mae(&utils::rescale(&s_a), &utils::rescale(&s_b));
-    let d_a = utils::percentiles(&utils::deltas(a), |&x| x);
-    let d_b = utils::percentiles(&utils::deltas(b), |&x| x);
-    let d_mae = utils::mae(&utils::rescale(&d_a), &utils::rescale(&d_b));
-    std::cmp::max_by(s_mae, d_mae, |x, y| {
-        x.partial_cmp(y)
-            .expect("`max_wmape_xs`: failed to compare floats")
-    })
-}
+// pub fn max_mae_rescaled_xs(a: &[Flow], b: &[Flow]) -> f64 {
+//     let s_a = utils::percentiles(a, |f| f.size);
+//     let s_b = utils::percentiles(b, |f| f.size);
+//     let s_mae = utils::mae(&utils::rescale(&s_a), &utils::rescale(&s_b));
+//     let d_a = utils::percentiles(&utils::deltas(a), |&x| x);
+//     let d_b = utils::percentiles(&utils::deltas(b), |&x| x);
+//     let d_mae = utils::mae(&utils::rescale(&d_a), &utils::rescale(&d_b));
+//     std::cmp::max_by(s_mae, d_mae, |x, y| {
+//         x.partial_cmp(y)
+//             .expect("`max_wmape_xs`: failed to compare floats")
+//     })
+// }
