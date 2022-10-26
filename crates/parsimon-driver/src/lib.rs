fd9396db498eaa5a8948abda78640b04fcaa383e
Kevin Zhao <kzhao0986@gmail.com>
2022-10-26 13:48:01 -0700

    sync

--- crates/parsimon-driver/src/lib.rs
+++ crates/parsimon-driver/src/lib.rs
@@ -1,59 +1,8 @@
-use std::fs;
 use std::path::{Path, PathBuf};
 
-use linksim_impls::ns3::Ns3Link;
-use parsimon_core::cluster::DefaultClustering;
 use parsimon_core::network::types::{Link, Node};
-use parsimon_core::network::{DelayNetwork, Flow, Network};
+use parsimon_core::network::{Flow, Network};
 use parsimon_core::units::{Bytes, Nanosecs};
-
-// TODO: remove me
-pub fn run_from_files(
-    network: impl AsRef<Path>,
-    flows: impl AsRef<Path>,
-) -> Result<DelayNetwork, Error> {
-    let network = read_network_spec(network)?;
-    let flows = read_flows(flows)?;
-    run(network, flows)
-}
-
-// TODO: remove me
-pub fn run(network: NetworkSpec, flows: Vec<Flow>) -> Result<DelayNetwork, Error> {
-    let spec = parsimon_core::Spec::builder()
-        .nodes(network.nodes)
-        .links(network.links)
-        .flows(flows)
-        .build();
-    let network = match network.linksim {
-        LinkSimKind::Ns3 {
-            root_dir,
-            ns3_dir,
-            window,
-            base_rtt,
-        } => {
-            fs::create_dir_all(&root_dir)?;
-            let linksim = Ns3Link::builder()
-                .root_dir(root_dir)
-                .ns3_dir(ns3_dir)
-                .window(window)
-                .base_rtt(base_rtt)
-                .build();
-            parsimon_core::run(spec, linksim, DefaultClustering)?
-        }
-    };
-    Ok(network)
-}
-
-// TODO: only used by clustering-basic... remove me
-pub fn read_network_spec(path: impl AsRef<Path>) -> Result<NetworkSpec, Error> {
-    let contents = std::fs::read_to_string(path.as_ref())?;
-    let network: NetworkSpec = match path.as_ref().extension().and_then(|ext| ext.to_str()) {
-        Some("json") => serde_json::from_str(&contents)?,
-        Some("dhall") => serde_dhall::from_str(&contents).parse()?,
-        _ => return Err(Error::UnknownFileType(path.as_ref().into())),
-    };
-    Ok(network)
-}
 
 // TODO: probably remove me
 pub fn read_network(topology_spec: impl AsRef<Path>) -> Result<Network, Error> {
@@ -99,13 +48,6 @@ pub enum Error {
 
     #[error("invalid topology")]
     Topology(#[from] parsimon_core::network::TopologyError),
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct NetworkSpec {
-    pub nodes: Vec<Node>,
-    pub links: Vec<Link>,
-    pub linksim: LinkSimKind,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
