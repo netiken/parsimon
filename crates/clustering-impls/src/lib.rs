4a83a5cd284d978b6a26d28bc5420550fc5dba40
Kevin Zhao <kzhao0986@gmail.com>
2024-04-10 22:13:51 -0700

    fix: remove dependency on nightly linked_list_cursors feature
    This will allow building parsimon with the stable release channel

--- crates/clustering-impls/src/lib.rs
+++ crates/clustering-impls/src/lib.rs
@@ -1,7 +1,6 @@
 //! This crate contains utilities for link clustering.
 
 #![warn(unreachable_pub, missing_debug_implementations, missing_docs)]
-#![feature(linked_list_cursors)]
 
 pub mod feature;
 pub mod greedy;
